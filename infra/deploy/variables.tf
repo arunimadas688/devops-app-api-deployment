a3d6d41475d783a84aec03fe1c48d1591423b26a
Arunima Das <arunimadas688@gmail.com>
2024-10-29 12:47:11 +0530

    move env to infra

---
+++ infra/deploy/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix for resources in AWS"
+  default     = "raa"
+}
+
+variable "project" {
+  description = "Project name for tagging resources"
+  default     = "devops-app-api"
+}
+
+variable "contact" {
+  description = "Contact email for tagging resources"
+  default     = "arunimadas688@gmail.com"
+}
